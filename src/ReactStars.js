320d60d64c357184c3dad2797f8852c72f8abbe3
Oleg Berman <bermanoleg@gmail.com>
2016-03-30 18:05:58 -0400

    simple star rendering

--- src/ReactStars.js
+++ src/ReactStars.js
@@ -4,11 +4,61 @@ class ReactStars extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      starCount: 5,
+      initialRating: 3,
+      size: 24,
+      altChar: '★',
+      showMode: false,
+      halfStars: true,
+      color: 'gray',
+      colorActive: ''
+    }
+
+    this.styleGeneric = {
+      fontSize: `${this.props.size}px`,
+      color: this.props.color
+    }
+
+    this.styleActive = {
+      color: '#ffd700'
+    }
+
+  }
+
+  componentWillMount() {
+    this.state.stars = []
+  }
+
+  componentDidMount() {
+    this.setState({
+      stars: this.getStars()
+    })
+  }
+
+  /** Returns an array of stars with their properties */
+  getStars() {
+    console.log('this.props.starCount', this.state.starCount);
+    let stars = []
+    for(let i = 0; i < this.state.starCount; i++) {
+      stars.push({
+        style: Object.assign(this.styleGeneric, this.styleActive),
+        html: `${this.state.altChar}`
+      })
+    }
+    return stars
+  }
+
+  renderStars() {
+    return this.state.stars.map((star) => {
+      return star.html
+    })
   }
 
   render() {
     return (
-      <div>Let's get started here test recompile</div>
+      <div>Here!{this.renderStars()}</div>
     )
   }
 
