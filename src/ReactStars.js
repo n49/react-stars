b228856caa65e30b537fe6590b628ddcedc0ec98
Oleg Berman <bermanoleg@gmail.com>
2016-03-30 18:42:18 -0400

    refactored

--- src/ReactStars.js
+++ src/ReactStars.js
@@ -3,26 +3,25 @@ import React, { Component } from 'react'
 class ReactStars extends Component {
 
   constructor(props) {
+
     super(props)
 
     this.state = {
-      starCount: 5,
-      initialRating: 3,
-      size: 24,
-      altChar: '★',
-      showMode: false,
-      halfStars: true,
-      color: 'gray',
-      colorActive: ''
+      value: props.value || 0
     }
 
-    this.styleGeneric = {
-      fontSize: `${this.props.size}px`,
-      color: this.props.color
-    }
-
-    this.styleActive = {
-      color: '#ffd700'
+    this.state.config = {
+      count:  props.starCount || 5,
+      size:   props.size || 24,
+      char:   props.char || '★',
+      edit:   props.edit,
+      half:   props.half,
+      // default color of inactive star
+      color1: props.color1 || 'gray',
+      // color of an active star
+      color2: props.color2 || '#ffd700',
+      // color color of a hovered star
+      color3: props.color3 || color2 || 'orange'
     }
 
   }
@@ -33,32 +32,41 @@ class ReactStars extends Component {
 
   componentDidMount() {
     this.setState({
-      stars: this.getStars()
+      stars: this.getArrayOfStars()
     })
   }
 
   /** Returns an array of stars with their properties */
-  getStars() {
-    console.log('this.props.starCount', this.state.starCount);
+  getArrayOfStars() {
     let stars = []
-    for(let i = 0; i < this.state.starCount; i++) {
+    for(let i = 0; i < this.state.config.count; i++) {
       stars.push({
-        style: Object.assign(this.styleGeneric, this.styleActive),
-        html: `${this.state.altChar}`
+        active: i <= this.state.value
       })
     }
     return stars
   }
 
   renderStars() {
-    return this.state.stars.map((star) => {
-      return star.html
+    const { color1, color2, size, char } = this.state.config
+    return this.state.stars.map((star, i) => {
+      const style = {
+        color: star.active ? color2 : color1,
+        fontSize: `${size}px`
+      }
+      return (
+        <span style={style} key={i}>
+          {char}
+        </span>
+      )
     })
   }
 
   render() {
     return (
-      <div>Here!{this.renderStars()}</div>
+      <div>
+        {this.renderStars()}
+      </div>
     )
   }
 
