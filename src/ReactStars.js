cd2592b2ad3732058782e113dc2335a7872abc3e
Oleg Berman <bermanoleg@gmail.com>
2016-03-30 16:16:45 -0400

    Initial commit

---
+++ src/ReactStars.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class ReactStars extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Let's get started here test recompile</div>
+    )
+  }
+
+}
+
+export default ReactStars
